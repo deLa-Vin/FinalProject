d4f9cee456b8cc41c3a6b4f7d14cb5870dad696f
aslusaf <aslusaf@live.com>
2022-05-25 14:42:55 -0600

    Topic entity created

---
+++ SkillGuildJPA/src/main/java/com/skilldistillery/skillguild/entities/Topic.java
@@ -0,0 +1,85 @@
+package com.skilldistillery.skillguild.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Topic {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	private String description;
+
+	@Column(name = "is_tech")
+	private boolean isTech;
+
+	public Topic() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public boolean isTech() {
+		return isTech;
+	}
+
+	public void setTech(boolean isTech) {
+		this.isTech = isTech;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Topic other = (Topic) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Topic [id=").append(id).append(", name=").append(name).append(", description=")
+				.append(description).append(", isTech=").append(isTech).append("]");
+		return builder.toString();
+	}
+
+}
