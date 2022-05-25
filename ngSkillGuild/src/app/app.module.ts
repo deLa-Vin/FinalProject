8e3b922182c5c87474f4dbf05d9a334a41f69000
Andy Cary <info@andycary.com>
2022-05-25 12:54:59 -0400

    Add home and navigation components, basic routing, bootstrap styles and component css

--- ngSkillGuild/src/app/app.module.ts
+++ ngSkillGuild/src/app/app.module.ts
@@ -5,10 +5,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
