7f325a88838da9097fc404c5b2b30f0636eb6ba0
aslusaf <aslusaf@live.com>
2022-05-25 15:12:08 -0600

    InteractionType entity created

---
+++ SkillGuildJPA/src/main/java/com/skilldistillery/skillguild/entities/InteractionType.java
@@ -0,0 +1,75 @@
+package com.skilldistillery.skillguild.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class InteractionType {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	@Column(name = "img_url")
+	private String imgUrl;
+
+	public InteractionType() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		InteractionType other = (InteractionType) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("InteractionType [id=").append(id).append(", name=").append(name).append(", imgUrl=")
+				.append(imgUrl).append("]");
+		return builder.toString();
+	}
+
+}
