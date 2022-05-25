54eb174777d3d56dfdf995bada399f41fac0ac7f
aslusaf <aslusaf@live.com>
2022-05-25 15:06:57 -0600

    Question entity created

---
+++ SkillGuildJPA/src/main/java/com/skilldistillery/skillguild/entities/Question.java
@@ -0,0 +1,75 @@
+package com.skilldistillery.skillguild.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Question {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String question;
+
+	@Column(name = "correct_answer")
+	private String correctAnswer;
+
+	public Question() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+
+	public String getCorrectAnswer() {
+		return correctAnswer;
+	}
+
+	public void setCorrectAnswer(String correctAnswer) {
+		this.correctAnswer = correctAnswer;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Question other = (Question) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Question [id=").append(id).append(", question=").append(question).append(", correctAnswer=")
+				.append(correctAnswer).append("]");
+		return builder.toString();
+	}
+
+}
