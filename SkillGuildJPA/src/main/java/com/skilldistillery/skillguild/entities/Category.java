0e4c651544b271461b76ee883bb5bca2f972bb4b
aslusaf <aslusaf@live.com>
2022-05-25 14:40:08 -0600

    Category entity created, persistence XML updates

---
+++ SkillGuildJPA/src/main/java/com/skilldistillery/skillguild/entities/Category.java
@@ -0,0 +1,85 @@
+package com.skilldistillery.skillguild.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Category {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String name;
+
+	private String description;
+
+	@Column(name = "img_url")
+	private String imgUrl;
+
+	public Category() {
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Category other = (Category) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Category [id=").append(id).append(", name=").append(name).append(", description=")
+				.append(description).append(", imgUrl=").append(imgUrl).append("]");
+		return builder.toString();
+	}
+
+}
--- SkillGuildJPA/src/main/java/com/skilldistillery/skillguild/entities/Guild.java
+++ SkillGuildJPA/src/main/java/com/skilldistillery/skillguild/entities/Guild.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+@Entity
 public class Guild {
 
 	@Id
