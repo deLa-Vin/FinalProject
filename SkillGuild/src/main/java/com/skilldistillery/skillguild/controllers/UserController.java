d3127e3882a1db688ab9b0336d3f54e70b4df540
Ving <ving@Vings-MacBook-Pro.local>
2022-05-26 12:42:25 -0400

    minor changes

--- SkillGuild/src/main/java/com/skilldistillery/skillguild/controllers/UserController.java
+++ SkillGuild/src/main/java/com/skilldistillery/skillguild/controllers/UserController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +40,23 @@ public class UserController {
 		return user;
 	}
 
+	@PostMapping("users/{uid}")
+	public User create(HttpServletRequest req, HttpServletResponse res, @RequestBody User user) {
+
+		try {
+			userSvc.create(user);
+			res.setStatus(201);
+			StringBuffer url = req.getRequestURL();
+			res.setHeader("Location", url.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+			res.setStatus(400);
+
+		}
+		return user;
+
+	}
+	
 	@DeleteMapping("users/{userId}")
 	public boolean deleteUser(
 			@PathVariable Integer userId,
