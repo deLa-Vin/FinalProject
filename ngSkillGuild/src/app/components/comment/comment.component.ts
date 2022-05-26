bf1331faf822adba3ee407155e2d1d9a76ff52cd
Alex Trill <alextrillaet@gmail.com>
2022-05-26 15:50:42 -0600

    Built out Comment CRUD methods on Angular side

--- ngSkillGuild/src/app/components/comment/comment.component.ts
+++ ngSkillGuild/src/app/components/comment/comment.component.ts
@@ -1,4 +1,7 @@
+import { Comment } from './../../models/comment';
+import { CommentService } from './../../services/comment.service';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-comment',
@@ -7,9 +10,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommentComponent implements OnInit {
 
-  constructor() { }
+  comments: Comment[] = [];
+
+  editComment: Comment | null = null;
+
+  selected: Comment | null = null;
+
+  newComment: Comment = new Comment();
+
+  constructor(
+    private commentSvc: CommentService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  getAllComments() {
+    this.commentSvc.index().subscribe(comments => {
+      this.comments = comments;
+    });
+  }
+
+  getCommentById(id: number) {
+    this.commentSvc.show(id).subscribe(
+      comment => {
+        return comment;
+      },
+      err => {
+        console.log(err);
+      }
+    )
+  }
+
+  createComment(uid: number, cid: number, comment: Comment): void {
+    this.commentSvc.create(uid, cid, comment).subscribe({
+      next: comment => {
+        console.log("Created successfully: " + comment.id);
+        this.comments.push(comment);
+        this.newComment = new Comment();
+      },
+      error: (err) => {
+        console.error('Error creating content: ', err);
+      }
+    })
+  }
+
+  updateComment(comment: Comment) {
+    this.commentSvc.update(comment).subscribe(
+      (data) => {
+        this.getAllComments();
+        this.editComment = null;
+        if (this.selected) {
+          this.selected = Object.assign({}, comment);
+        }
+      },
+      (err) => console.error(err)
+    );
+  }
+
+  deleteComment(id: number) {
+    this.commentSvc.delete(id).subscribe(
+      (data) => this.getAllComments(),
+      (err) => console.error(err)
+    );
+  }
 }
