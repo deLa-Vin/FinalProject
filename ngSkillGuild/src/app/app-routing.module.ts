84767e9e8f58b0c36fa4ac5a1df192eff358e6f3
Ving <ving@Vings-MacBook-Pro.local>
2022-05-26 17:04:07 -0400

    updated app routing module

--- ngSkillGuild/src/app/app-routing.module.ts
+++ ngSkillGuild/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CommentComponent } from './components/comment/comment.component';
 import { ContentComponent } from './components/content/content.component';
+import { FooterComponent } from './components/footer/footer.component';
 import { GuildComponent } from './components/guild/guild.component';
 import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { LogoutComponent } from './components/logout/logout.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
@@ -10,8 +16,15 @@ const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'user', component: UserComponent },
   { path: 'guild', component: GuildComponent },
+
+  { path: 'comment', component: CommentComponent },
+  { path: 'footer', component: FooterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'logout', component: LogoutComponent },
+  { path: 'navigation', component: NavigationComponent },
+
   { path: 'content', component: ContentComponent },
-  { path: '**', component: HomeComponent }
+  { path: '**', component: NotFoundComponent }
 ];
 
 @NgModule({
