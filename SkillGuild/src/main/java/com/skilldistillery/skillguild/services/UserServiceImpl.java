e86eb8e0f67f4a106c72b0fdb72caaddec910ebc
Ving <ving@Vings-MacBook-Pro.local>
2022-05-26 12:34:29 -0400

    updates to user controller

--- SkillGuild/src/main/java/com/skilldistillery/skillguild/services/UserServiceImpl.java
+++ SkillGuild/src/main/java/com/skilldistillery/skillguild/services/UserServiceImpl.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.skilldistillery.skillguild.entities.Guild;
 import com.skilldistillery.skillguild.entities.User;
 import com.skilldistillery.skillguild.repositories.UserRepository;
 
@@ -21,7 +22,7 @@ public class UserServiceImpl implements UserService {
 	}
 	
 	@Override
-	public User getUserById(int userId) {
+	public User show(int userId) {
 		Optional<User> userOpt = userRepo.findById(userId);
 		if (userOpt.isPresent()) {
 			return userOpt.get();
@@ -41,4 +42,16 @@ public class UserServiceImpl implements UserService {
 		}
 	}
 
-}
+	@Override
+	public User create(User newUser) {
+			return userRepo.saveAndFlush(newUser);
+		}
+
+
+	}
+
+	
+	
+	
+
+
