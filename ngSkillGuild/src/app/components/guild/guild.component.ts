21652d8be06007f1d383068609682de5b6fd0199
Andy Cary <info@andycary.com>
2022-05-26 11:42:24 -0400

    guild model, component, service, view setup

--- ngSkillGuild/src/app/components/guild/guild.component.ts
+++ ngSkillGuild/src/app/components/guild/guild.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Guild } from 'src/app/models/guild';
+import { GuildService } from 'src/app/services/guild.service';
 
 @Component({
   selector: 'app-guild',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GuildComponent implements OnInit {
 
-  constructor() { }
+  guilds: Guild[] = [];
+
+  constructor(
+    private guildSvc: GuildService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.guildSvc.index().subscribe(guilds => {
+      this.guilds = guilds;
+    });
+    console.log(this.guilds);
   }
 
 }
--- ngSkillGuild/src/app/models/guild.ts
+++ ngSkillGuild/src/app/models/guild.ts
@@ -1,2 +1,33 @@
 export class Guild {
+    id: number;
+    name: string;
+    description: string;
+    isPublic: boolean;
+    coverImg: string | null;
+    membershipCriteria: string;
+    createdByUserId: number;
+    createdOn: string;
+    lastUpdated: string;
+
+    constructor(
+        id: number = 0,
+        name: string = '',
+        description: string = '',
+        isPublic: boolean = false,
+        coverImg: string | null = null,
+        membershipCriteria: string = '',
+        createdByUserId: number = 0,
+        createdOn: string = '',
+        lastUpdated: string = ''
+    ) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.isPublic = isPublic;
+        this.coverImg = coverImg;
+        this.membershipCriteria = membershipCriteria;
+        this.createdByUserId = createdByUserId;
+        this.createdOn = createdOn;
+        this.lastUpdated = lastUpdated;
+    }
 }
