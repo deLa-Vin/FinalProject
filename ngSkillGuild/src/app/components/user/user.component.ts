90992ce1dfca187dc922129710c7fa3a229cdb86
Alex Trill <alextrillaet@gmail.com>
2022-05-26 10:52:58 -0600

    User model, service and component updated to show all users via table http://localhost:4202/user

--- ngSkillGuild/src/app/components/user/user.component.ts
+++ ngSkillGuild/src/app/components/user/user.component.ts
@@ -1,4 +1,8 @@
+import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user';
+import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-user',
@@ -7,9 +11,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  users: User[] = [];
+
+  selected: User | null = null;
+
+  constructor(
+    private userSvc: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.getAllUsers();
+
+  }
+  getAllUsers() {
+    this.userSvc.index().subscribe(users => {
+      this.users = users;
+    });
+  }
+
+  displayAll(): void {
+    this.selected = null;
+  }
+
+  getUserById(id: number) {
+    this.userSvc.show(id).subscribe(
+      guild => {
+        return guild;
+      },
+      err => {
+        console.log(err);
+      }
+    )
   }
 
 }
--- ngSkillGuild/src/app/services/user.service.ts
+++ ngSkillGuild/src/app/services/user.service.ts
@@ -1,9 +1,27 @@
+import { environment } from './../../environments/environment';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+private url = environment.baseUrl + 'v1/users/'
+
+private users: User[] = [];
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  index(): Observable<User[]> {
+    return this.http.get<User[]>(this.url);
+  }
+
+  show(id: number): Observable<User> {
+    return this.http.get<User>(this.url + id);
+  }
 }
