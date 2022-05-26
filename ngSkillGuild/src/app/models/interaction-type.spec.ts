dcc2f8f20752a1359dbad322201e03634a9ba793
Alex Trill <alextrillaet@gmail.com>
2022-05-26 11:25:47 -0600

    Models updated

---
+++ ngSkillGuild/src/app/models/interaction-type.spec.ts
@@ -0,0 +1,7 @@
+import { InteractionType } from './interaction-type';
+
+describe('InteractionType', () => {
+  it('should create an instance', () => {
+    expect(new InteractionType()).toBeTruthy();
+  });
+});
