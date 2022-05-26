b8bfba4b3fff7a59aed6d3e0373710ae292a4d94
aslusaf <aslusaf@live.com>
2022-05-26 11:34:43 -0600

    REST EP: GET statuses/{sid}

--- SkillGuild/src/main/java/com/skilldistillery/skillguild/controllers/StatusController.java
+++ SkillGuild/src/main/java/com/skilldistillery/skillguild/controllers/StatusController.java
@@ -2,12 +2,17 @@ package com.skilldistillery.skillguild.controllers;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skilldistillery.skillguild.entities.Guild;
 import com.skilldistillery.skillguild.entities.Status;
 import com.skilldistillery.skillguild.services.StatusService;
 
@@ -22,6 +27,18 @@ public class StatusController {
 	@GetMapping("statuses")
 	public List<Status> index() {
 		return statusServ.index();
+	}
+	
+	@GetMapping("statuses/{sid}")
+	public Status show(HttpServletRequest req, HttpServletResponse res, @PathVariable int sid) {
+
+		Status status = statusServ.show(sid);
+
+		if (status == null) {
+			res.setStatus(404);
+		}
+
+		return status;
 	}
 
 }
--- SkillGuild/src/main/java/com/skilldistillery/skillguild/services/StatusServiceImpl.java
+++ SkillGuild/src/main/java/com/skilldistillery/skillguild/services/StatusServiceImpl.java
@@ -1,6 +1,7 @@
 package com.skilldistillery.skillguild.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,18 @@ public class StatusServiceImpl implements StatusService {
 	@Override
 	public List<Status> index() {
 		return statusRepo.findAll();
+	}
+	
+	@Override
+	public Status show(int sid) {
+
+		Optional<Status> op = statusRepo.findById(sid);
+		if (op.isPresent()) {
+			Status result = op.get();
+			return result;
+		}
+
+		return null;
 	}
 
 }
