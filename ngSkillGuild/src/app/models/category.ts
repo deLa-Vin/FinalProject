145123d2b80cfc6b28d384bac59d99503a39050e
Alex Trill <alextrillaet@gmail.com>
2022-05-26 11:58:51 -0600

    VS models built out

--- ngSkillGuild/src/app/models/category.ts
+++ ngSkillGuild/src/app/models/category.ts
@@ -2,15 +2,18 @@ export class Category {
   id: number;
   name: string;
   description: string;
+  imgUrl: string;
 
 
   constructor(
     id: number = 0,
     name: string ="",
-    description: string =""
+    description: string ="",
+    imgUrl: string =""
   ){
 this.id =id;
 this.name = name;
 this.description = description;
+this.imgUrl = imgUrl;
   }
 }
--- ngSkillGuild/src/app/models/resource.ts
+++ ngSkillGuild/src/app/models/resource.ts
@@ -1,2 +1,20 @@
 export class Resource {
+  id: number;
+  title: string;
+  description: string;
+  resourceUrl: string;
+
+  constructor(
+    id: number=0,
+    title: string="",
+    description: string="",
+    resourceUrl: string=""
+
+  ){
+this.id=id;
+this.title = title;
+this.description = description;
+this.resourceUrl = resourceUrl;
+  }
+
 }
