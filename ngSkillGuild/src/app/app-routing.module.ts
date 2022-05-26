53dc8d73c5404a01d273fbbfb20ef3f5dfaeb305
Andy Cary <info@andycary.com>
2022-05-26 11:07:23 -0400

    add routes and use router-outlet in app-component.html

--- ngSkillGuild/src/app/app-routing.module.ts
+++ ngSkillGuild/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GuildComponent } from './guild/guild.component';
 import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
+  { path: 'user', component: UserComponent },
+  { path: 'guild', component: GuildComponent },
   { path: '**', component: HomeComponent }
 ];
 
