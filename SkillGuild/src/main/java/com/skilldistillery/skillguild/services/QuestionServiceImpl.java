3bedecc2afb969a94b6408eb7f032ff0831eae05
Ving <ving@Vings-MacBook-Pro.local>
2022-05-27 19:30:10 -0400

    updated question impl

--- SkillGuild/src/main/java/com/skilldistillery/skillguild/services/QuestionServiceImpl.java
+++ SkillGuild/src/main/java/com/skilldistillery/skillguild/services/QuestionServiceImpl.java
@@ -5,8 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.skilldistillery.skillguild.entities.Content;
 import com.skilldistillery.skillguild.entities.Question;
-import com.skilldistillery.skillguild.entities.Status;
+import com.skilldistillery.skillguild.repositories.ContentRepository;
 import com.skilldistillery.skillguild.repositories.QuestionRepository;
 
 
@@ -14,6 +15,9 @@ public class QuestionServiceImpl implements QuestionService {
 
 	@Autowired
 	QuestionRepository questionRepo;
+	
+	@Autowired
+	private ContentRepository contentRepo;
 	
 	@Override
 	public List<Question> index() {
@@ -32,7 +36,47 @@ public class QuestionServiceImpl implements QuestionService {
 		return null;
 	}
 
+	@Override
+	public Question create(int uid, Question question) {
+
+		Optional<Content> co = contentRepo.findById(uid);
+		if (co.isPresent()) {
+			Content content = co.get();
+			question.setContent(content);
+			return questionRepo.saveAndFlush(question);
+		}
+
+		return null;
+	}
+
+	@Override
+	public Question update(int qid, Question question) {
+		Optional<Question> co = questionRepo.findById(qid);
+		if (co.isPresent()) {
+			Question result = co.get();
+			question.setId(qid);
+			question.setContent(result.getContent());
+			return questionRepo.saveAndFlush(question);
+		}
+
+		return null;
+		
+	}
+
+	@Override
+	public boolean delete(int qid) {
+
+		Optional<Question> qu = questionRepo.findById(qid);
+		if (qu.isPresent()) {
+			questionRepo.deleteById(qid);
+			qu = questionRepo.findById(qid);
+			return !qu.isPresent();
+
+		}
+		return false;
+		
 	
+	}
 	
 	
 }
