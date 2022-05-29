42b6209b12538682a98e6050c6ee96ce588d57fb
Andy Cary <info@andycary.com>
2022-05-29 10:41:06 -0400

    show guild browser, details and all content

--- ngSkillGuild/src/app/hub/hub.component.ts
+++ ngSkillGuild/src/app/hub/hub.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Content } from '../models/content';
 import { Guild } from '../models/guild';
+import { ContentService } from '../services/content.service';
 import { GuildService } from '../services/guild.service';
 
 @Component({
@@ -11,11 +13,14 @@ import { GuildService } from '../services/guild.service';
 export class HubComponent implements OnInit {
 
   guilds: Guild[] = [];
+  selectedGuild: Guild | null = null;
 
-  selected: Guild | null = null;
+  contents: Content[] = [];
+  selectedContent: Content | null = null;
 
   constructor(
     private guildSvc: GuildService,
+    private contentSvc: ContentService,
     private route: ActivatedRoute,
     private router: Router
   ) {
@@ -25,10 +30,39 @@ export class HubComponent implements OnInit {
     this.getAllGuilds();
   }
 
+  // Guilds
   getAllGuilds() {
     this.guildSvc.index().subscribe(guilds => {
       this.guilds = guilds;
     });
+  }
+
+  displayAllGuilds(): void {
+    this.selectedGuild = null;
+  }
+
+  selectGuild(guild: Guild) {
+    this.getAllContents(); // Modify for individual guild
+    this.selectedGuild = guild;
+  }
+
+  // Content 
+
+  getAllContents() {
+    this.contentSvc.index().subscribe(contents => {
+      this.contents = contents;
+    });
+  }
+
+  getContentById(id: number) {
+    this.contentSvc.show(id).subscribe(
+      content => {
+        return content;
+      },
+      err => {
+        console.log(err);
+      }
+    )
   }
 
 }
